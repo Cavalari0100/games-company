0fba8ffb31c5535a7412739cc60f83d079418726
Cavalari0100 <joao.cavalarispa@hotmail.com>
2021-09-28 15:54:12 -0300

    teste1

--- games-company/src/componentes/paginas/firstPage.js
+++ games-company/src/componentes/paginas/firstPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
-import { Container,Col,Row } from 'reactstrap';
+import { Container } from 'reactstrap';
 
 class FirstPage extends Component {
     state={};
